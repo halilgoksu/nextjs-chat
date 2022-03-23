ab54d1a5497f3e381f285338b1eefd3099f5ec21
halil <halilskywater@gmail.com>
2022-03-23 16:16:19 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
+const Auth = () => {
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    if (username.length === 1 || secret.length === 1) return;
+
+    axios
+      .put(
+        "https://api.chatengine.io/users/",
+        { username, secret },
+        { headers: { "Private-Key": "791da6cc-cc04-4daf-8479-dfc68abb618c" } }
+      )
+
+      .then((r) => {
+        router.push("/chats");
+      });
+  }
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
+          <div className="auth-title">CaCa-Chat</div>
+
+          <div className="input-container">
+            <input
+              placeholder="Email or Sexy Nicname"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="UseYour Brain pls"
+              className="text-input"
+              onChange={(e) => setSecret(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Login/Sign Up
+          </button>
+        </form>
+      </div>
+      <div><p>By Goksu</p></div>
+
+    </div>
+  );
+};
+
+export default Auth;
